5866fb276671322a5c779a86a71ddac8dba15f7a
RedEye75 <yusufbinemamstk@gmail.com>
2023-05-27 00:26:55 +0600

    google fonts added

---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>this is header section of sheildpass website </h1>
+    </div>
+  );
+};
+
+export default Header;
